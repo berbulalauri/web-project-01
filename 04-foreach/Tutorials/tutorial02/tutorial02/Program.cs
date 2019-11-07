49ec13574e7d6de644f86a21b6399e878cbf9aa8
berbulalauri <gmberd002@gmail.com>
2019-11-07 15:10:07 +0400

    tutrial 2 to foerache

---
+++ 04-foreach/Tutorials/tutorial02/tutorial02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tutorial02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // int[,] array = { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+            int[,] array = new int [5,5];
+
+            for (int i=0; i<5; i++)
+            {
+                for(int j=0; j<5; j++)
+                {
+                    if (i == j) {
+                        array[i, j] = 1;
+                    } else
+                    {
+                        array[i, j] = 0;
+                    }
+                    Console.Write($"{array[i, j]}");
+
+                }
+                Console.WriteLine($"{Environment.NewLine}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
