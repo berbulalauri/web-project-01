8d8eac4279d5d8a780dbe870170559efb9a4e188
berbulalauri <gmberd002@gmail.com>
2019-11-07 18:06:00 +0400

    adding tut-02 to 05-method branch

---
+++ 05-methods/Tutorials/tutorial-02/tutorial-02/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace tutorial_02
+{
+    class Program
+    {
+
+        private static int PasswordLength = 8;
+        private static int digitCount = 2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the password: ");
+            var userPassword = Console.ReadLine();
+
+            var isPasswordValidx = isPasswordValid(userPassword, out var errorMessage);
+            var messageToOut = isPasswordValidx ? "Password is valid" : errorMessage;
+            Console.WriteLine(messageToOut);
+            Console.ReadLine();
+
+        }
+        public static bool isPasswordValid(string password, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+            if(password.Length < PasswordLength)
+            {
+                errorMessage += $"password must have at least eight characters. {Environment.NewLine}";
+            }
+
+            if (!isThisOnlyLettersOrDigit(password))
+            {
+                errorMessage += $"password must contain letter or digits. {Environment.NewLine}";
+
+            }
+            if (!doesPasswordContainsRequiredDigit(password, digitCount))
+            {
+                errorMessage += $"password must have at least two digits. {Environment.NewLine}";
+
+            }
+
+            return false;
+        }
+
+        private static bool isThisOnlyLettersOrDigit(string password)
+        {
+            foreach(var c in password)
+            {
+                if (!char.IsLetter(c) || !char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool doesPasswordContainsRequiredDigit(string password, int requiredDigitCount)
+        {
+            int counter = 0;
+            foreach (var c in password)
+            {
+                if (char.IsDigit(c)) {counter++;}
+
+                if (counter >= requiredDigitCount) return true;
+            }
+            return false;
+        }
+    }
+}
