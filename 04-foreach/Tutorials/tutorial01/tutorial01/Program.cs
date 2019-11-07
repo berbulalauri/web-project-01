08dd7be41f4a8abd8e712be31b76f38970993258
berbulalauri <gmberd002@gmail.com>
2019-11-07 14:51:07 +0400

    adding 04-foreach tutorial 01 with VS

---
+++ 04-foreach/Tutorials/tutorial01/tutorial01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace tutorial01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input first number: ");
+            var firstInput = Console.ReadLine();
+            var parsingResult = int.TryParse(firstInput, out var firstValue);
+
+            if (!parsingResult)
+            {
+                Console.WriteLine("Incorect Input... ");
+                return;
+            }
+            Console.WriteLine("please input second number: ");
+            var secondInput = Console.ReadLine();
+            parsingResult = int.TryParse(secondInput, out var secondValue);
+
+            if (!parsingResult)
+            {
+                Console.WriteLine("Incorect Input... ");
+                return;
+            }
+            if (firstValue > secondValue)
+            {
+                Console.WriteLine("range can't be calculated");
+            }
+            for (int i = firstValue + 1; i < secondValue; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
