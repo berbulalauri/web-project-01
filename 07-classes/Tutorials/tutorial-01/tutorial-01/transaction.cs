ac95b4af4d0734eb50ce97bc70e24802439873ea
berbulalauri <gmberd002@gmail.com>
2019-11-08 18:14:59 +0400

    adding tutorial-01 to 07 -classes branch

---
+++ 07-classes/Tutorials/tutorial-01/tutorial-01/transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tutorial_01
+{
+    public class transaction
+    {
+        public decimal Amount { get; set;}
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+        public transaction(decimal amount, string note = "")
+        {
+            this.Amount = amount;
+            Date = DateTime.Now;
+        }
+    }
+}
