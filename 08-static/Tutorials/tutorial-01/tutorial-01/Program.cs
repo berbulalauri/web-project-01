f82d8d31f0938431faa2b216f48f608d920fc24d
berbulalauri <gmberd002@gmail.com>
2019-11-11 15:26:09 +0400

    adding 08-statics tut-01

---
+++ 08-static/Tutorials/tutorial-01/tutorial-01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace tutorial_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someString = "here example";
+            var commentedString = someString.RemoveSecondWordsPart();
+
+            Console.WriteLine(commentedString);
+        }
+
+    }
+    public static class ExtensionMethods
+    {
+        public static string AddHtmlComment(this string currentString)
+        {
+            return $"<-- {currentString}-->";
+        }
+        public static string CapitalizeFirstLetter(this string source)
+        {
+            if (!string.IsNullOrWhiteSpace(source))
+            {
+                return $"{char.ToUpper(source[0])}{source.Substring(1, source.Length-1)}"; 
+            }
+            return "";
+        }
+        public static string RemoveSecondWordsPart(this string source)
+        {
+            if (!string.IsNullOrWhiteSpace(source))
+            {
+                return $"{source.Substring(0,source.Length/2)}"; 
+            }
+            return "";
+        }
+
+
+    }
+
+}
