d94396600689feaab573b4f0228f6a9fd8d9afe7
berbulalauri <gmberd002@gmail.com>
2019-11-14 16:32:49 +0400

    addint 15-files 1-2 tutorials

---
+++ 15-files/Tutorials/tutorial-01/tutorial-01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace tutorial_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringdata = @"Y'all act like you never seen a white person before
+Jaws all on the floor like Pam like Tommy just burst in the door
+And started whoopin' her ass worse than before
+They first were divorced, throwin' her over furniture (Agh!)";
+
+            FileInfo readableFile = new FileInfo(@"../../../tutorialresult.txt");
+            try
+            {
+                using (FileStream fileStream = readableFile.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read))
+                {
+                    var data = Encoding.Default.GetBytes(stringdata);
+                    fileStream.Write(data, 10, data.Length);
+
+                }
+                //using StreamWriter sw = new StreamWriter(fileinfo.FullName);
+                /*                foreach (var ch in stringdata)
+                                {
+                                    sw.Write(ch);
+                                }
+                */
+
+
+                Console.WriteLine("finish writing file");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"something going wrong {ex.Message}");
+            }
+
+
+        }
+    }
+}
---
+++ 15-files/Tutorials/tutorial-02/tutorial-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace tutorial_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileInfo file=new FileInfo(@"../../../tutorialresult.txt");
+            Console.WriteLine(file);
+            try
+            {
+                List<string> list = new List<string>();
+                var fs = file.OpenRead();
+                using(var streamreader=new StreamReader(fs))
+                {
+                    string line;
+                    while((line = streamreader.ReadLine()) != null)
+                    {
+                        list.Add(line);
+                    };
+                    list.ForEach(Console.Write);
+                }
+
+            } catch( Exception ex)
+            {
+
+            }
+        }
+    }
+}
