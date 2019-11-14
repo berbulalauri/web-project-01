7e3e0ec0400f10eb5019e64c0707d6e804613175
berbulalauri <b.bulalauri@gmail.com>
2019-11-14 10:52:17 +0400

    adding: 08-static p-2; 09-oop p-1; 10-delegates p-2 & 4

---
+++ 08-static/Practices/practice-02/practice-02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace practice_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("John Doe", 19);
+            Student s2 = new Student("Oliver Smith", 20);
+            Console.WriteLine("Number of Instance is: "+Student.CountOfIntances());
+        }
+
+    }
+    public class Student
+    {
+        public static int initNumber = 0;
+        public static int increasedNumber;
+
+        public Student(string a, int b)
+        {
+            increasedNumber = ++initNumber;
+        }
+        public static int CountOfIntances()
+        {
+            return increasedNumber;
+        }
+    }
+}
