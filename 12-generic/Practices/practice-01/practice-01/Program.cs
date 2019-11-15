655648871731f86009de133f9d5c501e82d49bf1
berbulalauri <b.bulalauri@gmail.com>
2019-11-15 10:55:08 +0400

    Adding: 10-del's practice-3;11-null's practice-3;12-generic practice-1-2-4

---
+++ 12-generic/Practices/practice-01/practice-01/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace practice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Choose the operation: ");
+            Console.WriteLine("1. Array of number: ");
+            Console.WriteLine("2. Array of strings: ");
+
+            int.TryParse(Console.ReadLine(), out int elect);
+            var callMethod = new CaseMethods();
+
+            switch (elect){
+                case 1:
+                    callMethod.Case1();
+                break;
+                case 2:
+                    callMethod.Case2();
+                    break;
+                default:
+
+                break;
+            }
+        }
+    }
+    public class CaseMethods
+    {
+        public void Case1()
+        {
+            try
+            {
+                Console.WriteLine("Please Write the line (ONLY NUMBERS): ");
+                var userInput = Console.ReadLine();
+                List<double> listOfDouble = userInput.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => double.TryParse(x, out double val) ? val : 1).ToList();
+                CountEqualValues<double> point2 = new CountEqualValues<double>(listOfDouble);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Case2()
+        {
+            try
+            {
+                Console.WriteLine("Please Write the line (STRING / NUMBERS): ");
+                var userInput = Console.ReadLine();
+                List<string> listOfDouble = userInput.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                CountEqualValues<string> point2 = new CountEqualValues<string>(listOfDouble);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+    class CountEqualValues<T>
+    {
+        public CountEqualValues(List<T> list)
+        {
+            try
+            {
+                var query = list.GroupBy(x => x)
+                  .Where(g => g.Count() >= 1)
+                  .Select(y => new { Element = y.Key, Counter = y.Count() })
+                  .ToList();
+
+                foreach (var items in query)
+                {
+                    Console.WriteLine($" {items.Element} has been called {items.Counter} times");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ 12-generic/Practices/practice-04/practice-04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace practice_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storage = new Storage<int>();
+            var stringStorage = new Storage<string>("my test");
+            var dateStorage = new Storage<DateTime>(DateTime.Now);
+
+            Console.WriteLine(storage);
+            Console.WriteLine(stringStorage);
+            Console.WriteLine(dateStorage);
+        }
+    }
+    public class Storage<T>
+    {
+        public T X { get; set; }
+        public DateTime Y { get; set; }
+        public Storage() { }
+        public Storage(T value)
+        {
+            X = value;
+        }
+        public Storage(DateTime value)
+        {
+            Y = value;
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)} : {X} ";
+        }
+    }
+}
