da0a0fffd6e0d7e60dd38c7af87b782b34eb7094
berbulalauri <gmberd002@gmail.com>
2019-11-15 16:34:45 +0400

    adding 16-format tut-1

--- 16-format/Tutorials/tutorial-01/tutorial-01/Program.cs
+++ 16-format/Tutorials/tutorial-01/tutorial-01/Program.cs
@@ -9,7 +9,27 @@ namespace tutorial_01
     {
         static void Main(string[] args)
         {
+            DateTime date = DateTime.Now;
             CultureInfo cultureInfo = new CultureInfo("ka-GE");
+            Console.Write("{0,0:d}\t",date);
+            Console.Write("{0,10:D}\t",date);
+            Console.Write("{0,20:yyyy}\t",date);
+            Console.Write("{0,40:U}\t",date);
+
+            var geoStyle = string.Format(cultureInfo, "{0:n}",25437.5342);
+
+            Console.WriteLine("\n\a");
+            Console.WriteLine("{0:c}",23.33);
+            Console.WriteLine("{0:e}",230000000.35);
+            Console.WriteLine("{0:f1}",2333.335);
+            Console.WriteLine("{0:g}",2333.322395);
+            Console.WriteLine("{0:n}",2516453.3972235);
+
+            Console.WriteLine("{0:X2}",251645396);
+            Console.WriteLine("Georian Style: "+geoStyle);
+
+
+/*            CultureInfo cultureInfo = new CultureInfo("ka-GE");
             DateTime dt = DateTime.Now;
             Console.WriteLine("Current date: {0,0:d} Number {1,10:F}", dt, 2);
             var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
@@ -19,7 +39,7 @@ namespace tutorial_01
             decimal number = 100000;
             string bekamoney = number.ToString("c", culture);
             Console.WriteLine(bekamoney);
-            
+*/            
             //Console.WriteLine(date.ToShortDateString());
         }
     }
