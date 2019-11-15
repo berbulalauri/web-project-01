39eca47b30f4e240742c6166ae2ba5d36ec8224c
berbulalauri <b.bulalauri@gmail.com>
2019-11-16 00:07:41 +0400

    adding 15 pracitce1-2-3-4; 14 practice-2

---
+++ 15-files/Practices/practice-01/practice-01/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+class Test
+{
+    public static void Main()
+    {
+        Console.WriteLine("===========================================");
+        Console.WriteLine("1. HELP     Commands list");
+        Console.WriteLine("2. DISK     Displays detailed disks information");
+        Console.WriteLine("3. EXIT     Exit");
+        Console.WriteLine("===========================================");
+
+        bool isTrue = true;
+        while (isTrue)
+        {
+            Console.Write(">");
+            var userInput = Console.ReadLine();
+
+            if (userInput == "help")
+            {
+                Console.WriteLine("===========================================");
+                Console.WriteLine("HELP     Commands list");
+                Console.WriteLine("DISK     Displays detailed disks information");
+                Console.WriteLine("EXIT     Exit");
+                Console.WriteLine("===========================================");
+            }
+            else if(userInput == "disk")
+            {
+                consoleWriter();
+                txtWriteMethod();
+            } else if(userInput == "exit")
+            {
+                isTrue = false;
+            }
+        }
+    }
+
+    public static void consoleWriter()
+    {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                Console.WriteLine("Name {0}", d.Name);
+                Console.WriteLine("Type: {0}", d.DriveType); 
+                if (d.IsReady == true)
+                {
+                    Console.WriteLine("File system: {0}", d.DriveFormat);
+                    Console.WriteLine("Total size of drive:   {0, 15} bytes ",d.TotalSize);
+                    Console.WriteLine("Total available space: {0, 15} bytes\n",d.TotalFreeSpace);
+                }
+            }
+    }  
+    public static void txtWriteMethod()
+    {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+            StreamWriter writer;
+ 
+            FileStream file = new FileStream("../../../practice1.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            var standardOutput = Console.Out;
+            using (writer = new StreamWriter(file))
+            {
+                    Console.SetOut(writer);
+
+            foreach (DriveInfo d in allDrives)
+            {
+                Console.WriteLine("Name {0}", d.Name);
+                Console.WriteLine("Type: {0}", d.DriveType); 
+                if (d.IsReady == true)
+                {
+                    Console.WriteLine("File system: {0}", d.DriveFormat);
+                    Console.WriteLine("Total size of drive:   {0, 15} bytes ",d.TotalSize);
+                    Console.WriteLine("Total available space: {0, 15} bytes\n",d.TotalFreeSpace);
+                }
+            }
+                Console.SetOut(standardOutput);
+            }
+    }
+}
---
+++ 15-files/Practices/practice-02/practice-02/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace practice_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool whileloop = true;
+            string pathFinder = "";
+            Console.Write(">");
+
+            while (whileloop)
+            {
+
+                try
+                {
+                    var userCmd = Console.ReadLine();
+                    List<string> listOfDouble = userCmd.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                    int listLength = listOfDouble.Count;
+
+                    if (listOfDouble[0] == "cd") { pathFinder = listOfDouble[listLength - 1]; Console.Write($"{pathFinder}>"); }
+
+                    if (listOfDouble[0] == "dir") { consoleClass(pathFinder); txtWriterClass(pathFinder); Console.Write($"{pathFinder}>"); }
+                    
+                    else if (listOfDouble[0] == "exit")
+                    {
+                        whileloop = false;
+                    }
+                    if (listOfDouble[0] != "cd" && listOfDouble[0] != "dir" && listOfDouble[0] != "exit")
+                    {
+                        Console.WriteLine($"Your Command is Wrong, Please Try Again!");
+                        Console.Write($"{pathFinder}>");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+        public static void txtWriterClass(string dir)
+        {
+            StreamWriter writer;
+            try
+            {
+                FileStream file = new FileStream("../../../practice2.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                var standardOutput = Console.Out;
+                using (writer = new StreamWriter(file))
+                {
+                    Console.SetOut(writer);
+
+                    foreach (string f in Directory.GetFiles(dir))
+                    {
+                        FileInfo fi = new FileInfo(f);
+                        var created = fi.CreationTime;
+                        var lastmodified = fi.LastWriteTime;
+                        Console.WriteLine($"{created}\t{lastmodified}\t{Path.GetFileName(f)}");
+
+                    }
+                    foreach (string d in Directory.GetDirectories(dir))
+                    {
+                        FileInfo fi = new FileInfo(d);
+                        var created = fi.CreationTime;
+                        var lastmodified = fi.LastWriteTime;
+                        Console.WriteLine($"{created}\t{lastmodified}\t{Path.GetFileName(d)}");
+                    }
+                    Console.SetOut(standardOutput);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void consoleClass(string dir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                {
+                    FileInfo fi = new FileInfo(f);
+                    var created = fi.CreationTime;
+                    var lastmodified = fi.LastWriteTime;
+                    Console.WriteLine($"{created}\t{lastmodified}\t{Path.GetFileName(f)}");
+
+                }
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    FileInfo fi = new FileInfo(d);
+                    var created = fi.CreationTime;
+                    var lastmodified = fi.LastWriteTime;
+                    Console.WriteLine($"{created}\t{lastmodified}\t{Path.GetFileName(d)}");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
