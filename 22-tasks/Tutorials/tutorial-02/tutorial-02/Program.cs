6d72085df256a634886c84ed85b13e9e34eae3d5
berbulalauri <gmberd002@gmail.com>
2019-11-20 18:00:41 +0400

    adding tut1-2 of 22-tasks

---
+++ 22-tasks/Tutorials/tutorial-02/tutorial-02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace tutorial_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int breakIndex = 3;
+            var random = new Random();
+            var locker = new object();
+
+            Parallel.For(1, 20, (i, state) => {
+                Console.WriteLine($"Begining Iteration {i}");
+
+                lock (locker)
+                {
+                    var delay = random.Next(500, 3000);
+                    Task.Delay(delay).GetAwaiter().GetResult();
+
+                }
+
+                if (i == breakIndex)
+                {
+                    Console.WriteLine($"Breaking Iteration {i}");
+                    state.Break();
+                }
+                Console.WriteLine($"Compliting Iteration {i}");
+            });
+        }
+    }
+}
