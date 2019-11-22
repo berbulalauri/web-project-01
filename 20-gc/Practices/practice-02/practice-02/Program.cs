f1a93b9526113167e1fa5f607124f6174f698669
berbulalauri <b.bulalauri@gmail.com>
2019-11-22 23:45:47 +0400

    adding 20 p-2-3; 21 p1-4; 22 p-3-4

---
+++ 20-gc/Practices/practice-02/practice-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace myhomework
+{
+    public class Resources
+    {
+        public int id { get; set; }
+        public Resources(int x)
+        {
+            id = x;
+        }
+        ~Resources()
+        {
+            Console.WriteLine($"Resource {id} was created");
+            Console.ReadKey();
+        }
+    }
+
+    class Procedure
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Important notice: Some version of .NET Core can't corectlly compile GC destructor.\nin order to see the result. Please compile in only .NET Framework or copy code to repl.it \n");
+            // Console.WriteLine($"max generation of resource {GC.MaxGeneration}");
+            int x = 1; // int x = GC.MaxGeneration;
+
+            Resources resources = new Resources(x);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        }
+
+    }
+
+}
