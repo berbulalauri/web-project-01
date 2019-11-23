787b120a5fb9f2fda1f332b69781cabaa4e3db77
berbulalauri <b.bulalauri@gmail.com>
2019-11-23 09:49:21 +0400

    adding only Project 1

---
+++ Projects/project-01/project-01/PasswordCoverClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace project_01
+{
+    class PasswordCoverClass
+    {
+        public const string namePattern = @"^[\p{L} \.\-]{3,1115}$";
+        private const string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\[{\]};:<>|./?,-])(?=.*[^\da-zA-Z]).{6,12}$";
+
+        public string pass { get; set; }
+        public void passwordCover()
+        {
+            do
+            {
+                pass = "";
+                Console.Write("\nWrite the password: ");
+                do
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        this.pass += key.KeyChar;
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                        {
+                            pass = pass.Substring(0, (pass.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                        else if (key.Key == ConsoleKey.Enter)
+                        {
+                            Console.WriteLine();
+                            break;
+                        }
+                    }
+                } while (true);
+
+                if (pass.Length > 6 && validateFirstName(pass)) { break; } else { Console.WriteLine("YOUR PASSWORD IS INCORRECT. PLEASE TRY AGAIN!"); }
+            }
+            while (true);
+        }
+
+        public string hashedPassRtnr()
+        {
+            return pass;
+        }
+        public static bool validateFirstName(string frsname)
+        {
+            return Regex.IsMatch(frsname, passwordPattern);
+        }
+
+    }
+
+
+}
+
+
