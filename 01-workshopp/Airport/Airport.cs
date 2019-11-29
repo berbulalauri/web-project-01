a697954318135fb29aab14977e33b6baff0cb5ee
berbulalauri <b.bulalauri@gmail.com>
2019-11-29 16:54:18 +0400

    add airport project

---
+++ 01-workshopp/Airport/Airport.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using workshop_02.Tariffs;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using workshop_02.Helpers;
+using workshop2.Helpers;
+
+namespace workshop_02
+{
+    public class Airport
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public bool IsInternational { get; set; }
+        public string BindedAirLineToAirport { get; set; }
+
+        public Airport()
+        {
+        }
+        public Airport(string city, string name, bool isInternational, string bindedAirLineToAirport)
+        {
+            Name = name;
+            City = city;
+            IsInternational = isInternational;
+            BindedAirLineToAirport = bindedAirLineToAirport;
+            //Tariff = isInternetNeed ? (ITariff)new ZipperTariff() : (ITariff)new CheetahTariff();
+        }
+        public override string ToString()
+        {
+            var printAnswer = IsInternational ? "International" : "";
+            var BindedString = (BindedAirLineToAirport != null) ? $"Airline which is operating in {City} is: {BindedAirLineToAirport.ToUpper()}" : "";
+
+
+            return $"{Name}\n" +
+                $"{BindedString}";
+            //return $"{printAnswer} airport in {City}: {Name} \n" +
+            //    $"{BindedString}";
+        }
+    }
+}
+
+
---
+++ 01-workshopp/Airport/ValidationHelper.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using workshop_02.Repositories;
+using System.Linq;
+
+namespace workshop_02
+{
+    public class ValidationHelper
+    {
+        private const decimal _minimumNumber = 3;
+        private const decimal _minSubscriberAge = 14;
+        private const string namePattern = @"^[\p{L} \.\-]{2,1115}$";
+        private const string emailPattern = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+
+        public static bool IsNameValid(string name)
+        {
+            if (name.Length >= _minimumNumber)
+            {
+                if (Regex.IsMatch(name, namePattern))
+                {
+                    return name.Length >= _minimumNumber;
+                }
+            }
+            return false;
+        }
+        public static bool validateFirstName(string frsname)
+        {
+            return Regex.IsMatch(frsname, namePattern);
+        }
+
+
+        public static bool IsPassportIsValid(string passportId)
+        {
+            return new Regex(@"^[A-z]{2}[0-9]{6}$").IsMatch(passportId);
+        }
+
+        public static bool IsDateOfBirthValid(DateTime dob)
+        {
+            return DateTime.Now.Year - dob.Year >= _minSubscriberAge;
+        }
+
+        public static bool IsPhoneNumberUnique(string phoneNumber)
+        {
+            //var jsonRepo = new JsonFileRepository<List<Airport>>(Configuration.FileName);
+
+            //var subscribersList = jsonRepo.Read();
+
+            //if (subscribersList != null)
+            //{
+            //    return !subscribersList.Any(s => s.phoneNumber == phoneNumber);
+            //}
+
+            return true;
+
+        }
+    }
+}
+
