c539d135886615e041b57dd9cf4af03dd7daff51
berbulalauri <b.bulalauri@gmail.com>
2019-12-05 16:09:24 +0400

    addint 05-ico Tutorial 1-2

---
+++ 05-ioc/Tutorials/tutorial-02/tutorial-02/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial_02.Models
+{
+    public class Brand
+    {
+       public string Name { get; set; }
+    }
+}
---
+++ 05-ioc/Tutorials/tutorial-02/tutorial-02/Services/brandService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tutorial_02.Models;
+
+namespace tutorial_02.Services
+{
+    public class brandService : IBrandService
+    {
+        public brandService()
+        {
+
+        }
+        public IEnumerable<Brand> GetBrands()
+        {
+            return new List<Brand>
+            {
+                new Brand { Name = "BMW" },
+                new Brand { Name = "JapanMotor" },
+                new Brand { Name = "RR" }
+            };
+        }
+
+    }
+}
