e6cd08dfb50c847e3fa9a1857f766506159876f2
berdia bulalauri <b.bulalauri@gmail.com>
2019-12-07 12:00:33 +0000

    adding: 05-ioc P-2; updating 03-startup's P-2, Previous version had a bug. so I fixed and updating it.

---
+++ 03--startup/Practices/practice-02/practice-02/Books1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practice_02
+{
+    public class Books
+    {
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int  Year { get; set; }
+    
+    }
+}
+
+//public Books(string name, string autor, int year)
+//{
+//    Name = name;
+//    Author = autor;
+//    Year = year;
+//}
---
+++ 05-ioc/Practices/practice-02/practice-02/Services/IGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practice_02.Services
+{
+    public interface IGenerator
+    {
+        //IEnumerable<Brand> GetBrands();
+        int GeneratePositiveIntegerNumber();
+        int GenerateNegativeIntegerNumber();
+        string GenerateString(int lenght);
+    }
+}
