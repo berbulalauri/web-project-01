ce894f89792d730f9fa689cad56f84107c39132a
berbulalauri <b.bulalauri@gmail.com>
2019-12-10 20:42:06 +0400

    adding: 08-taghelper tutorial-1-2

---
+++ 08-taghelpers/Tutorials/tutorial-01/tutorial-01/Services/Concrete/CarRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tutorial_01.Models;
+using tutorial_01.Services.Abstract;
+
+namespace tutorial_01.Services.Concrete
+{
+    public class CarRepository : IRepository<Car>
+    {
+        List<Car> _cars = new List<Car>();
+        public void Add(Car item)
+        {
+            _cars.Add(item);
+        }
+
+        public IEnumerable<Car> GetAll()
+        {
+            return _cars;
+        }
+    }
+}
---
+++ 08-taghelpers/Tutorials/tutorial-01/tutorial-01/Services/Concrete/CarService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tutorial_01.Models;
+using tutorial_01.Services.Abstract;
+
+namespace tutorial_01.Services.Concrete
+{
+    public class CarService : ICarService
+    {
+        IRepository<Car> _repository;
+
+        public CarService(IRepository<Car> repository)
+        {
+            _repository = repository;
+        }
+        public void AddCar(Car car)
+        {
+            _repository.Add(car);
+        }
+
+        public List<Car> GetCars()
+        {
+            var cars = _repository.GetAll();
+            return cars.OrderBy(x => x.MaxSpeed).ToList();
+
+        }
+    }
+}
