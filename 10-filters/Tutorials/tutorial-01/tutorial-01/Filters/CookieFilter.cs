d8a485da07adea6d97f02ec75f29e715d26b2b71
berbulalauri <b.bulalauri@gmail.com>
2019-12-11 21:41:01 +0400

    adding 10-filters tutorial 1-2

---
+++ 10-filters/Tutorials/tutorial-01/tutorial-01/Filters/CookieFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial_01.Filters
+{
+    public class CookieFilter : IActionFilter
+    {
+        readonly string _lastVisitedKey = "LastVisited";
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        context.HttpContext.Response.Cookies.Append(_lastVisitedKey, DateTime.Now.ToString("mm.dd.yyyy-hh.mm.ss"));
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Request.Query.ToList().ForEach(
+                q=>
+                {
+                    context.HttpContext.Response.Cookies.Append(q.Key,q.Value);
+                }
+                );
+        }
+    }
+}
