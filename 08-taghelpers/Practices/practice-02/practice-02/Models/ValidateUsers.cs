bc0903642091cc2ae475d6eb3751f94e89f0bafe
berbulalauri <b.bulalauri@gmail.com>
2019-12-12 21:14:16 +0400

    saving 08 taghelper practice 2

---
+++ 08-taghelpers/Practices/practice-02/practice-02/Models/ValidateUsers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practice_02.Models
+{
+    public class ValidateUsers
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
